2dcb75af6e95017623b278f9aa915bec7d77bdb1
Carlos Grisales <41649792+carlos0320@users.noreply.github.com>
2021-04-07 16:53:19 -0500

    Ejercicio 3

--- actividad1.js
+++ actividad1.js
@@ -4,3 +4,10 @@ console.log( 'Hello World' );
 // Ejercicio2
 let resultado = 3 + 5;
 console.log( resultado );
+
+
+// Ejercicio 3
+let nombre = "Carlos"
+let apellido = "Grisales"
+
+console.log( nombre + " " + apellido );
